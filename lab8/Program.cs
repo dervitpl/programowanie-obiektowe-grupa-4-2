847ac34f1cd89636d09b5e5e7eefed3a53cff884
Kacper Mucha <kacper.mucha@microsoft.wsei.edu.pl>
2022-04-27 12:01:53 +0200

    .

---
+++ lab8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 4, 6, 7, 3, 2, 8, 9 };
+            IEnumerable<int> evenNumbers =
+                from n in ints
+                where n % 2 == 0
+                select n;
+            Console.WriteLine(string.Join(", ", evenNumbers));
+            Console.WriteLine("hello world");
+        }
+    }
+}
