e479e6c2372bb7f17fa4cfcfa42ad67ca37bb15a
dervitpl <kacpermucha420@gmail.com>
2022-06-07 19:54:27 +0200

    splash screen dodany oraz wybrany pomysl na api

---
+++ Pogodynka/SplashScreen.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Pogodynka
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        DispatcherTimer dT = new DispatcherTimer();
+        public SplashScreen()
+        {
+            InitializeComponent();
+            dT.Tick += new EventHandler(dT_Tick);
+            dT.Interval = new TimeSpan(0, 0, 5);
+            dT.Start();
+        }
+
+        private void dT_Tick(object sender, EventArgs e)
+        {
+            DashBoard dB = new DashBoard();
+            dB.Show();
+            dT.Stop();
+            this.Close();
+        }
+    }
+}
